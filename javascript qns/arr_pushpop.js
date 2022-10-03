0144b98a33f9f3d82d79299ba404cb90c7e446f4
233759-Nishad <113493480+233759-Nishad@users.noreply.github.com>
2022-10-03 17:55:44 +0530

    change

---
+++ javascript qns/arr_pushpop.js
@@ -0,0 +1,10 @@
+const fruits = ["kiwi", "Orange", "Apple", "anar"];
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.push("Grapes")
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
+fruits.unshift("berry");
+console.log(fruits);
