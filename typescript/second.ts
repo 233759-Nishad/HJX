e60deef2b04d4e8529ddd22e70fcf0cff282deb9
233759-Nishad <113493480+233759-Nishad@users.noreply.github.com>
2022-10-18 17:59:46 +0530

    change

---
+++ typescript/second.ts
@@ -0,0 +1,9 @@
+class Course {
+    Course_Name: String;
+    Course_Code: number;
+    duration: number;
+    Students_ID: number[] Getcoursedetails() {
+    // write further codes
+    let Tableau = new Course();
+    }
+    }
